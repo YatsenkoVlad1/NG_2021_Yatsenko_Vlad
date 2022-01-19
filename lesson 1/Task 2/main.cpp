882b11084f8caac4e347425930bb29c6fa66bdee
Влад Яценко <>
2022-01-19 03:29:38 +0300

    used cout function

---
+++ lesson 1/Task 2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "             ___  "<< endl;
+    cout << "       |     | |  " << endl;
+    cout << "      / \\    | | " << endl;
+    cout << "     |--Î|===|-|  " << endl;
+    cout << "     |---|   |d|  " << endl;
+    cout << "    /     \\  |w| "<< endl;
+    cout << "   | U     | |b|  " << endl;
+    cout << "   | S     |=| |  " << endl;
+    cout << "   | A|    | | |  "<< endl;
+     cout <<"   |_______| |_|  " << endl;
+    cout << "    |@| |@|  | |  " << endl;
+    cout << " ____________|_|__" << endl;
+    return 0;
+}
